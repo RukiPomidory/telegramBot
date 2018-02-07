8f45b1f2836ef9c10192a486e9f53bd2e9a67549
Arseniy <Arseniy>
2018-02-07 22:34:15 +0300

    buttons_test

--- Source.py
+++ Source.py
@@ -1,13 +1,21 @@
-
 import telebot
 import config
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(func=lambda x: True, content_types='text')
+@bot.message_handler(func=lambda x: x.text == 'Нет', content_types='text')
 def send_no(message):
     bot.reply_to(message, 'Нет')
+
+
+@bot.message_handler(commands=['button'])
+def test(message):
+    markup = types.InlineKeyBoardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    markup.add(url_button)
+    bot.send_message(message.chat.id, 'TEST', reply_markup=markup)
 
 
 if __name__ == '__main__':
