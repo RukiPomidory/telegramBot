8987679e35290c742a6f62c73ec3b38ad922f217
Arseniy <Arseniy>
2018-02-07 21:15:04 +0300

    'all_messages'

--- Source.py
+++ Source.py
@@ -5,7 +5,7 @@ import config
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(func=lambda x:x=='Нет',content_types='text')
+@bot.message_handler(func=lambda x: True, content_types='text')
 def send_no(message):
     bot.reply_to(message, 'Нет')
 
